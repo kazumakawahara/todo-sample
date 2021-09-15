1771a1432066407c300aac18adfd4a78dde68207
Yusuke Taira <ney07214545@icloud.com>
2021-09-15 14:00:19 +0900

    main.goを作成

--- cmd/todo-sample/main.go
+++ cmd/todo-sample/main.go
@@ -1,1 +1,13 @@
 package main
+
+import (
+	"log"
+
+	"github.com/kazumakawahara/todo-sample/infrastructure/router"
+)
+
+func main() {
+	if err := router.Run(); err != nil {
+		log.Fatalln(err)
+	}
+}
